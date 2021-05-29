c8a741dc526d419fa28c01535ec153a21aa696dc
Rahmad Firmansyah <38803182+rfirmansyh@users.noreply.github.com>
2021-05-29 21:25:44 +0700

    Add Title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import store from '../app/store';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/app.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+        <Provider store={store}>
+            <Head>
+                <title>My page title</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            </Head>
+            <Component {...pageProps} />
+        </Provider>
   )
 }
 
