0e3a422399705302ecc9d991ebbec8c3f7444d34
Rahmad Firmansyah <38803182+rfirmansyh@users.noreply.github.com>
2021-05-29 21:38:50 +0700

    Add Favicon and Title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
         <Provider store={store}>
             <Head>
-                <title>My page title</title>
+                <title>Wirakerja</title>
+
+                <link rel="icon" type="image/png" href="/img/favicon-16x16.png"/>
+                <link rel="icon" type="image/png" href="/img/favicon-32x32.png"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
             </Head>
